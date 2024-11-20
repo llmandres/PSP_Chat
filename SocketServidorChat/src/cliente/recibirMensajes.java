118112096c892b720f2e6ca34e1baa368bfcba66
Yolmi <yolminey@gmail.com>
2024-11-20 20:18:06 +0100

    5

--- SocketServidorChat/src/cliente/recibirMensajes.java
+++ SocketServidorChat/src/cliente/recibirMensajes.java
@@ -5,38 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class recibirMensajes implements Runnable{
-	
-	private  boolean activo = true;
-	Socket socket;
-	
-	public recibirMensajes(Socket socket) {
-		this.socket = socket;
-	}
-
-	@Override
-	public void run() {
-        while (activo) {
-        	try {
-   	         String serverMessage;
-   	         InputStreamReader entrada = new InputStreamReader(socket.getInputStream());
-   	         BufferedReader entradaBuffer = new BufferedReader(entrada);
-             while (!Thread.currentThread().isInterrupted()) {
-                 if ((serverMessage = entradaBuffer.readLine()) != null) {
-                     System.out.println(serverMessage);
-                 } else {
-                     break; 
-                 }
-             }
-        	}catch(Exception e) {
-        		
-        	}
-        }
-        
-		
-	}
-    public void detener() {
-        activo = false;  
+public class recibirMensajes implements Runnable {
+    
+    private Socket socket;
+    
+    public recibirMensajes(Socket socket) {
+        this.socket = socket;
     }
 
+    @Override
+    public void run() {
+  
+        try (InputStreamReader entrada = new InputStreamReader(socket.getInputStream());
+             BufferedReader entradaBuffer = new BufferedReader(entrada)) {
+
+            String serverMessage;
+
+            while (!Thread.currentThread().isInterrupted()) {
+                if ((serverMessage = entradaBuffer.readLine()) != null) {
+                    System.out.println(serverMessage); 
+                } else {
+                    break; 
+                }
+            }
+        } catch (IOException e) {
+            if (!Thread.currentThread().isInterrupted()) {
+
+                System.err.println("CLIENTE: Error al recibir mensaje: " + e.getMessage());
+            }
+        } 
+    }
 }
